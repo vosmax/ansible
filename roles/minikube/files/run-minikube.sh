b1caf08a63ce81660a0a4f7571ccd0f0fa7f9fef
Maxim Voskresenskiy <maxim.voskresenskiy@uptick.com>
2019-03-17 18:31:36 +0300

    Init

---
+++ roles/minikube/files/run-minikube.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+# chown -R $USER $HOME/.kube &&\
+# chgrp -R $USER $HOME/.kube &&\
+# chown -R $USER $HOME/.minikube &&\
+# chgrp -R $USER $HOME/.minikube &&\
+export MINIKUBE_WANTUPDATENOTIFICATION=false &&\
+export MINIKUBE_WANTREPORTERRORPROMPT=false &&\
+export CHANGE_MINIKUBE_NONE_USER=true &&\
+export MINIKUBE_HOME=$HOME &&\
+export KUBECONFIG=$HOME/.kube/config &&\
+export KUBECONFIG=$HOME/.kube/config &&\
+minikube start \
+--vm-driver=none \
+--apiserver-ips=$(ifconfig eth0 | grep netmask | awk '{print $2}') \
+--apiserver-names=$(ifconfig eth0 | grep netmask | awk '{print $2}') \
+--extra-config=kubelet.resolv-conf=/var/run/systemd/resolve/resolv.conf && \
+chown -R $USER $HOME/.kube && \
+chown -R $USER $HOME/.minikube || true
